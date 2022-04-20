a50e63d1f160d71b2928ff06994ff93e7460541d
Paciente8159 <Paciente8159@users.noreply.github.com>
2022-04-20 16:45:31 +0100

    Updated tmcdriver.h

--- src/tmcdriver.h
+++ src/tmcdriver.h
@@ -26,39 +26,37 @@ extern "C"
 
 #include <stdint.h>
 
+#define TMC_READ_ERROR 0xFFFFFFFFUL
 #define GCONF 0x00
 #define IHOLD_IRUN 0x10
 #define CHOPCONF 0x6C
 #define TPWMTHRS 0X13
 #define DRV_STATUS 0x6F
 
-    typedef uint8_t (*tmc_getc)(void);
-    typedef uint8_t (*tmc_putc)(uint8_t);
-    typedef void (*tmc_initdriver)(void);
+    typedef void (*tmc_rw)(uint8_t *, uint8_t, uint8_t);
+    typedef void (*tmc_startup)(void);
 
     typedef struct
     {
         // identifies the type of driver (2208, 2209, 2225, etc...)
-        uint8_t type;
+        uint16_t type;
         // if the driver has a slave ID this should be set here
         uint8_t slave;
-        // Callback for the UART/SPI interface initialization
-        tmc_initdriver init;
-        // Callback for UART getc. If the driver uses SPI, this callback should disable the CS pin
-        tmc_getc read;
-        // Callback for UART putc. If the driver uses SPI, this callback should enable the CS pin and transmit a spi byte
-        tmc_putc write;
+        // Callback for the UART/SPI interface initialization if needed
+        tmc_startup init;
+        // Callback for RW
+        tmc_rw rw;
     } tmc_driver_t;
 
     void tmc_init(tmc_driver_t *driver);
     float tmc_get_current(tmc_driver_t *driver, float rsense);
     void tmc_set_current(tmc_driver_t *driver, float current, float rsense, float ihold_mul);
-    uint8_t tmc_get_microstep(tmc_driver_t *driver);
-    void tmc_set_microstep(tmc_driver_t *driver, uint8_t ms);
+    int32_t tmc_get_microstep(tmc_driver_t *driver);
+    void tmc_set_microstep(tmc_driver_t *driver, int16_t ms);
     uint8_t tmc_get_stepinterpol(tmc_driver_t *driver);
     void tmc_set_stepinterpol(tmc_driver_t *driver, uint8_t enable);
     uint32_t tmc_get_stealthshop(tmc_driver_t *driver);
-    void tmc_set_stealthshop(tmc_driver_t *driver, uint32_t value);
+    void tmc_set_stealthchop(tmc_driver_t *driver, uint32_t value);
     uint32_t tmc_get_status(tmc_driver_t *driver);
     uint32_t tmc_read_register(tmc_driver_t *driver, uint8_t address);
     uint32_t tmc_write_register(tmc_driver_t *driver, uint8_t address, uint32_t val);
